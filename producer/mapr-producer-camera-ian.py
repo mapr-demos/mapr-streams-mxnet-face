1661696a2d1ec3ddf18c80252e22db07c6eb2420
iandow <kayaking@gmail.com>
2018-08-08 11:02:03 -0700

    config for gcloud demo

---
+++ producer/mapr-producer-camera-ian.py
@@ -0,0 +1,28 @@
+from mapr_streams_python import Producer
+import numpy as np
+import cv2, time, pickle
+
+p = Producer({'streams.producer.default.stream': '/mapr/gcloud.cluster.com/tmp/rawvideostream'})
+cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH,480)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT,360)
+
+while (cap.isOpened):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    # Our operations on the frame come here
+    image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    ret, jpeg = cv2.imencode('.png', image)
+    # Display the resulting frame
+    cv2.imshow('frame',frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+    p.produce('topic1', jpeg.tostring(), str(cap.get(cv2.CAP_PROP_POS_MSEC)))
+    print("video position: "+str(cap.get(cv2.CAP_PROP_POS_MSEC))+"ms")
+    time.sleep(1)
+
+p.flush()
+cap.release()
+cv2.destroyAllWindows()
